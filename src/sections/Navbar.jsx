72205e1886d497d9724435c3a6b512a63d0d8345
RiyanIQ <riyan.iftikhar.qureshi@gmail.com>
2024-12-24 15:23:42 -0800

    Finalized dynamic Navbar to display separately for both Mobile and Website users

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
+import { navLinks } from '../constants/index.js';
 
 const NavItems = () => {
     return(
         <ul className='nav-ul'>
-
+            {navLinks.map(({id, href, name}) => (
+                <li key={id} className='nav-li'>
+                    <a href={href} className='nav-li_a' onClick={() => {}}>
+                        {name}
+                    </a>
+                </li>
+            ))}
         </ul>
     )
 }
@@ -20,14 +27,22 @@ const Navbar = () => {
                     Riyan
                 </a>
 
-                <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle menu'>
+                {/* sm:hidden prevents this button from being visible on non-small devices */}
+                <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle menu'> 
                     <img src={isOpen ? "assets/close.svg" : 'assets/menu.svg'} alt='toggle' className='w-6 h-6'/>
                 </button>
 
-                <nav className='sm:flex hidden'>
+                {/* Somehow it hides it on small devices but when the humberger menu button dissapears it is able to appear */}
+                <nav className='hidden sm:flex'>
                     <NavItems/>
                 </nav>
             </div>
+        </div>
+
+        <div className={`nav-sidebar ${isOpen ? 'max-h-screen':'max-h-0'}`}>
+            <nav className='p-5'>
+                <NavItems/>
+            </nav>
         </div>
     </header>
   )
