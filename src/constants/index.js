ca91f90d9c7b23e1878ffc03d04c94f03c25b7d0
RiyanIQ <riyan.iftikhar.qureshi@gmail.com>
2024-12-26 23:56:32 -0800

    Added 3D Objects with Animations to surround HackerRoom

--- src/constants/index.js
+++ src/constants/index.js
@@ -248,12 +248,13 @@ export const navLinks = [
   
   export const calculateSizes = (isSmall, isMobile, isTablet) => {
     return {
-      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
-      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
-      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
-      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
-      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+      deskScale: isSmall ? 0.07 : isMobile ? 0.08 : 0.1,
+      deskPosition: isMobile ? [0.5, -6.5, 0] : [0.8, -9, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [9, -1, 0] : isTablet ? [15, -3, 0] : [15, -3.5, 0],
+      pokePosition: isSmall ? [4, -5, 0] : isMobile ? [8, -7, 0] : isTablet ? [12, -10, 0] : [12, -10, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [8, 4, 0] : isTablet ? [12, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-17, 8, 0] : isTablet ? [-23, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -8, -10] : isMobile ? [-12, -9, -10] : isTablet ? [-15, -16, -10] : [-17, -16, -10],
     };
   };
   
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,46 +5,59 @@ import React, { Suspense } from 'react'
 import HackerRoom from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
 import { Leva, useControls } from 'leva'
+import { useMediaQuery } from 'react-responsive'
+import { calculateSizes } from '../constants'
+import Target from '../components/Target.jsx'
+import ReactLogo from '../components/ReactLogo.jsx'
+import PokeBall from '../components/PokeBall.jsx'
+import Cube from '../components/Cube.jsx'
+import Rings from '../components/Rings.jsx'
 
 const Hero = () => {
     // Creating a control panel with sliders using Leva to adjust 3D properties like rotation, size, and position 
-  const controls = useControls('HackerRoom', {
-    positionX: {
-        value: 0.8,
-        min: -10,
-        max: 10
-    },
-    positionY: {
-        value: -10,
-        min: -10,
-        max: 10
-    },
-    positionZ: {
-        value: 2,
-        min: -10,
-        max: 10
-    },
-    rotationX: {
-        value: 0,
-        min: -10,
-        max: 10
-    },
-    rotationY: {
-        value: -Math.PI,
-        min: -10,
-        max: 10
-    },
-    rotationZ: {
-        value: 0,
-        min: -10,
-        max: 10
-    },
-    scale: {
-        value: 0.1,
-        min: 0.1,
-        max: 10
-    }
-});
+//   const controls = useControls('HackerRoom', {
+//     positionX: {
+//         value: 0.8,
+//         min: -10,
+//         max: 10
+//     },
+//     positionY: {
+//         value: -9,
+//         min: -10,
+//         max: 10
+//     },
+//     positionZ: {
+//         value: 2,
+//         min: -10,
+//         max: 10
+//     },
+//     rotationX: {
+//         value: 0,
+//         min: -10,
+//         max: 10
+//     },
+//     rotationY: {
+//         value: -Math.PI,
+//         min: -10,
+//         max: 10
+//     },
+//     rotationZ: {
+//         value: 0,
+//         min: -10,
+//         max: 10
+//     },
+//     scale: {
+//         value: 0.1,
+//         min: 0.1,
+//         max: 10
+//     }
+// });
+// useMediaQuery() lets you check whether current device dimensions conform to specified dimension boundaries 
+const isSmall = useMediaQuery({maxWidth: 440});
+const isMobile = useMediaQuery({maxWidth: 768});
+const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+
+const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
@@ -59,21 +72,31 @@ const Hero = () => {
         </div>
 
         <div className='w-full h-full absolute inset-0'>
-            <Leva />
+            {/* <Leva /> */}
             {/* When you have components in the canvas remember they are all in Threejs not jsx */}
             <Canvas className='w-full h-full'>
                 <Suspense fallback={<CanvasLoader/>}>
 
                     <PerspectiveCamera makeDefault position={[0,0,30]}/>
-
+                    
                     <HackerRoom 
-                        // scale={0.07}
-                        // position={[0,0,0]}
-                        // rotation={[0, 0, 0]}
-                        position={[controls.positionX, controls.positionY, controls.positionZ]}
-                        rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
-                        scale={[controls.scale, controls.scale, controls.scale]}
+                        scale={sizes.deskScale}
+                        position={sizes.deskPosition}
+                        rotation={[0, -Math.PI, 0]}
+
+                        // Leva Object Orientation Controls
+                        // position={[controls.positionX, controls.positionY, controls.positionZ]}
+                        // rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
+                        // scale={[controls.scale, controls.scale, controls.scale]}
                     />
+
+                    <group>
+                        <Target position={sizes.targetPosition}/>
+                        <ReactLogo position={sizes.reactLogoPosition}/>
+                        <PokeBall position={sizes.pokePosition}/>
+                        <Cube position={sizes.cubePosition}/>
+                        <Rings position={sizes.ringPosition}/>
+                    </group>
                     
                     <ambientLight intensity={1}/>
                     <directionalLight position={[10,10,10]} intensity={0.5} color={0xF0F0F0}/>
